e5bafcde5f0ff6b40e99c3a8cb541b399b304d9a
fedi41-prog <crocofedi4@gmail.com>
2026-02-27 20:07:40 +0100

    c

--- bots/CrocoBot/CrocoBot.py
+++ bots/CrocoBot/CrocoBot.py
@@ -1,7 +1,9 @@
+import math
+
 from robocode_tank_royale.bot_api import Color, BulletState
 from robocode_tank_royale.bot_api.bot import Bot
 from robocode_tank_royale.bot_api.events import ScannedBotEvent, HitByBulletEvent, BulletFiredEvent, BotDeathEvent, \
-    TickEvent
+    TickEvent, Condition, CustomEvent
 import asyncio
 
 GREEN = Color.from_rgb(0x00, 0xFF, 0x00)
@@ -25,6 +27,7 @@ class CrocoBot(Bot):
         self.enemies: dict[int, tuple[ScannedBotEvent, list[str]]] = {}
         self.target_bot: ScannedBotEvent = None
         self.color_tick = 0
+        self.spotted_enemies = 0
 
     def run(self) -> None:
         """Called when a new round is started -> initialize and do some movement."""
@@ -35,15 +38,15 @@ class CrocoBot(Bot):
         self.scan_color = GREEN
         self.bullet_color = GREEN
 
+        ticks_before_scan = 3
+
         # Repeat while the bot is running
         while self.running:
-            self.set_turn_radar_right(120)
-
+            #self.target_speed = 5
             facing_at_us = self.get_all_with_tag("facing at us")
             if len(facing_at_us) > 0:
-                print(len(facing_at_us))
                 self.aim_at(facing_at_us[0][0])
-                self.set_fire(1)
+                self.set_fire(2)
 
                 self.radar_color = RED
                 self.turret_color = RED
@@ -51,24 +54,51 @@ class CrocoBot(Bot):
                 self.radar_color = GREEN
                 self.turret_color = GREEN
 
-                self.init_fast_scan()
+                self.align_radar()
+                ticks_before_scan -= 1
+
+                self.update_radar()
+
+            if self.can_hit_any_bot(): self.set_fire(2)
             self.go()
 
-    def init_fast_scan(self):
-        self.set_turn_gun_right(60)
-        self.set_turn_right(60)
-        self.set_turn_radar_right(60)
+    def can_hit_any_bot(self):
 
-    def stop_fast_scan(self):
-        self.set_turn_gun_right(0)
-        self.set_turn_right(0)
-        self.set_turn_radar_right(0)
+        for i in self.enemies.keys():
+            e = self.enemies[i][0]
+            if self.can_hit(e.x, e.y):
+                print("can shoot", e.scanned_bot_id)
+                return True
+
+        return False
+
+    def can_hit(self, x, y, radius=18):
+        dist = distance_point_to_line((self.x, self.y), self.gun_direction, (x, y))
+        return dist <= radius
+
+    def align_radar(self):
+        bearing = self.calc_radar_bearing(self.gun_direction)
+        self.set_turn_radar_left(bearing)
+
+
+    def stop_radar(self):
+        self.radar_turn_rate = 0
+
+    def update_radar(self):
+        if self.gun_turn_rate == 0:
+            self.gun_turn_rate = self.max_gun_turn_rate
+        if self.spotted_enemies >= self.enemy_count:
+            self.gun_turn_rate *= -1
+            self.spotted_enemies = 0
 
 
     def aim_at(self, bot:ScannedBotEvent):
         bearing = self.gun_bearing_to(bot.x, bot.y)
+
         self.set_turn_gun_left(bearing/2)
         self.set_turn_left(bearing/2)
+        bearing2 = self.bearing_to(bot.x, bot.y)
+        self.set_turn_left(90 - bearing2)
 
 
     def update_color(self):
@@ -88,25 +118,35 @@ class CrocoBot(Bot):
 
         return res
 
-
     def on_scanned_bot(self, e: ScannedBotEvent) -> None:
         """We saw another bot -> fire!"""
         #d = self.direction_to(e.x, e.y)
         #self.set_turn_gun_right(d)
 
+        self.spotted_enemies += 1
+        self.update_radar()
+
         enemy_direction = e.direction
-        enemy_to_bot_direction = (self.direction_to(e.x, e.y) + 180) % 360
+        enemy_to_bot_direction = self.direction_to(e.x, e.y)
         #print(enemy_direction, enemy_to_bot_direction)
 
         tags = []
 
-        if enemy_to_bot_direction - 10 < enemy_direction < enemy_to_bot_direction + 10:
+        bullet_laser_dist = distance_point_to_line((e.x, e.y), enemy_direction, (self.x, self.y))
+        print(e.scanned_bot_id, bullet_laser_dist)
+
+        if bullet_laser_dist <= 18:
             tags.append("facing at us")
             print("danger")
-            self.stop_fast_scan()
+
+
 
         self.enemies[e.scanned_bot_id] = (e, tags)
         self.target_bot = e
+
+        self.aim_at(e)
+
+        if self.can_hit_any_bot(): self.set_fire(2)
 
 
         #bearing = self.bearing_to(float(e.x), float(e.y))
@@ -133,8 +173,10 @@ class CrocoBot(Bot):
         """We were hit by a bullet -> turn perpendicular to the bullet."""
         # Calculate the bearing to the direction of the bullet
         bearing = self.calc_bearing(e.bullet.direction)
+        bearing2 = self.calc_gun_bearing(e.bullet.direction)
         # Turn 90 degrees to the bullet direction based on the bearing
         self.turn_right(90 - bearing)
+        self.turn_gun_right(180 - bearing2)
 
 
     def on_tick(self, tick_event: TickEvent) -> None:
@@ -142,6 +184,35 @@ class CrocoBot(Bot):
         pass
 
 
+
+
+
+def distance_point_to_line(line_point, line_angle_deg, target_point):
+    """
+    Berechnet den Abstand eines Punktes zu einer Geraden.
+
+    :param line_point: Tuple (x0, y0) Punkt auf der Geraden
+    :param line_angle_deg: Winkel der Geraden in Grad (Robocode-style)
+    :param target_point: Tuple (x1, y1) Punkt, zu dem der Abstand berechnet wird
+    :return: Abstand als float
+    """
+    x0, y0 = line_point
+    x1, y1 = target_point
+
+    # Robocode: 0° = nach oben, 90° = rechts
+    # konvertieren zu Standard-Koordinaten (0° = rechts, counter-clockwise)
+    theta_rad = math.radians(90 - line_angle_deg)
+
+    # Richtungsvektor der Geraden
+    dx = math.cos(theta_rad)
+    dy = math.sin(theta_rad)
+
+    # Abstand = |(P - A) x d| / |d|
+    # Kreuzprodukt in 2D: (x1-x0, y1-y0) x (dx, dy) = (x1-x0)*dy - (y1-y0)*dx
+    numerator = abs((x1 - x0) * dy - (y1 - y0) * dx)
+    denominator = math.hypot(dx, dy)  # ||d||, hier =1, da normiert
+    distance = numerator / denominator
+    return distance
 
 
 async def main() -> None:
