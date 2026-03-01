85ae3c7bf98581d761dd99368751abe8c4791493
fedi41-prog <crocofedi4@gmail.com>
2026-03-01 09:03:23 +0100

    c

--- bots/CrocoBotV2/CrocoBotV2.py
+++ bots/CrocoBotV2/CrocoBotV2.py
@@ -1,5 +1,6 @@
 import asyncio
 import math
+import time
 
 from robocode_tank_royale.bot_api import Color, BulletState
 from robocode_tank_royale.bot_api.bot import Bot
@@ -31,10 +32,9 @@ STATE_TARGET = 3
 class CrocoBotV2(Bot):
     def __init__(self):
         super().__init__()
-        self.bullets: list[BulletState] = []
-        self.enemies: dict[int, tuple[ScannedBotEvent, list[str]]] = {}
-        self.target: ScannedBotEvent = None
-        self.color_tick = 0
+        self.enemies: dict[int, tuple[ScannedBotEvent, dict]] = {}
+
+        self.target_id: int = 0
         self.spotted_enemies: set[int] = set()
         self.move_direction = 1
 
@@ -50,21 +50,50 @@ class CrocoBotV2(Bot):
         self.scan_color = GREEN
         self.bullet_color = GREEN
 
-        self.turn_radar_left(10)
+
+        self.enemies: dict[int, tuple[ScannedBotEvent, dict]] = {}
+        self.target_id: int = 0
+        self.spotted_enemies: set[int] = set()
+        self.move_direction = 1
+        self.bot_state = STATE_IDLE
+
 
         # Repeat while the bot is running
         while self.running:
             self.update_color()
             if self.bot_state == STATE_IDLE:
-                self.rotate_perpendicular_to_enemies()
+                #self.rotate_perpendicular_to_enemies()
                 self.gun_turn_rate = self.max_gun_turn_rate
+
+                #self.forward(50 * self.move_direction)
+
+                self.set_forward(500)
+                self.set_turn_left(90)
+
+                self.go()
+
+                #self.move_direction *= -1
+            if self.bot_state == STATE_TARGET:
+                self.gun_turn_rate = self.max_gun_turn_rate
+                self.rotate_perpendicular_to_bot(self.target_id)
 
                 self.forward(50 * self.move_direction)
                 self.move_direction *= -1
 
 
+
     # =====================
     # =====================
+
+    def rotate_perpendicular_to_bot(self, bot_id):
+        b = self.enemies[bot_id][0]
+        bearing = self.bearing_to(b.x, b.y)
+        if bearing > 90:
+            bearing = 180 - bearing
+        if bearing <= -90:
+            bearing = 180 + bearing
+
+        self.turn_left(bearing)
 
     def rotate_perpendicular_to_enemies(self):
 
@@ -87,13 +116,12 @@ class CrocoBotV2(Bot):
         if bearing <= -90:
             bearing = 180 + bearing
 
-        print(bearing)
         self.turn_left(abs(bearing))
 
 
     def aim_at(self, bot:ScannedBotEvent):
         bearing = self.gun_bearing_to(bot.x, bot.y)
-        self.set_turn_gun_left(bearing)
+        self.turn_gun_left(bearing)
 
 
     # =====================
@@ -109,48 +137,93 @@ class CrocoBotV2(Bot):
 
         enemy_direction = e.direction
         enemy_to_bot_direction = self.direction_to(e.x, e.y)
+        distance = self.distance_to(e.x, e.y)
 
-        tags = []
+        data = {
+            "bullet_hit_count":0
+        }
+        if e.scanned_bot_id in self.enemies:
+            data = self.enemies[e.scanned_bot_id][1]
 
-        # tags hinzufügen
 
-        self.enemies[e.scanned_bot_id] = (e, tags)
+
+        self.enemies[e.scanned_bot_id] = (e, data)
 
         if self.bot_state == STATE_IDLE:
-            direction_diff = abs(enemy_direction - enemy_to_bot_direction)
-            if direction_diff < 10 or direction_diff < 45 and e.speed == 0:
-                self.fire(3)
-            elif direction_diff < 45: #Feind schlecht positioniert
+            if self.gun_heat == 0:
+
+                direction_diff = abs(enemy_direction - enemy_to_bot_direction)
+                firepower = 0
+
+                if direction_diff < 10 or (direction_diff < 45 and e.speed == 0) or self.distance_to(e.x, e.y) < 50:
+                    firepower = 3
+                elif direction_diff < 45:
+                    firepower = 2
+                elif e.speed == 0:
+                    firepower = 1
+
+
+                if firepower > 0:
+                    self.aim_at(e)
+                    self.fire(firepower)
+        if self.bot_state == STATE_TARGET:
+            if e.scanned_bot_id == self.target_id:
+                self.aim_at(e)
                 self.fire(2)
-            else:
-                self.fire(1)
+     
+
 
 
     def on_bot_death(self, e: BotDeathEvent) -> None:
         if e.victim_id in self.enemies.keys():
             self.enemies.pop(e.victim_id)
 
-        if self.target.scanned_bot_id == e.victim_id:
-            self.target = None
+        if self.target_id == e.victim_id:
+            self.target_id = 0
+            self.change_state(STATE_IDLE)
+
+    def on_hit_by_bullet(self, e: HitByBulletEvent) -> None:
+        bot = e.bullet.owner_id
+        if bot in self.enemies:
+            hit_count = self.get_bot_data(bot, "bullet_hit_count") + 1
+            self.set_bot_data(bot, "bullet_hit_count", hit_count)
+            print(f"hit by {bot} X {hit_count}")
+            if hit_count > 1:
+                self.target_id = bot
+                self.change_state(STATE_TARGET)
+
+                print("targeting:", bot)
+
+
+        else:
+            print("hit by unknown bot!")
+
 
 
     # =====================
     # =====================
 
-    def get_all_with_tag(self, tag:str) -> list[tuple[ScannedBotEvent, list]]:
-        res = []
-        for i in self.enemies.keys():
-            e = self.enemies[i]
-            if tag in e[1]:
-                res.append(e)
+    def set_bot_data(self, bot_id, data_key, value):
+        if bot_id in self.enemies:
+            self.enemies[bot_id][1][data_key] = value
+        else:
+            print("UNKNOWN BOT")
+    def get_bot_data(self, bot_id, data_key):
+        if bot_id in self.enemies and data_key in self.enemies[bot_id][1]:
+            return self.enemies[bot_id][1][data_key]
+        else:
+            return None
 
-        return res
     def is_near_wall(self, margin=18 * 3):
         if margin < self.x < self.arena_width - margin and margin < self.y < self.arena_height - margin:
             return False
         return True
     def on_tick(self, tick_event: TickEvent) -> None:
         pass
+
+    def change_state(self, new_state):
+        self.bot_state = new_state
+        self.update_color()
     def update_color(self):
         if self.bot_state == STATE_IDLE:
             self.radar_color = GREEN
@@ -164,6 +237,8 @@ class CrocoBotV2(Bot):
         elif self.bot_state == STATE_TARGET:
             self.radar_color = RED
             self.turret_color = RED
+
+
 
 
 def weighted_average(values, weights) -> float:
